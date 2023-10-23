a0941204a77977f1ab3ced2652d02020fc658795
Howrad <9yiwei@gmail.com>
2023-10-23 11:42:47 +0800

    Day 42: Add payment-creditcard method

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 df = pd.read_csv("hotels.csv", dtype={"id": str})
+df_card = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
-
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
         self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
@@ -24,7 +24,6 @@ class Hotel:
 
 
 class ReservationTicket:
-
     def __init__(self, costumer_name, hotel_object):
         self.costumer_name = costumer_name
         self.hotel = hotel_object
@@ -39,14 +38,30 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, numbers):
+        self.numbers = numbers
+
+    def validate(self, expiration, cvc, holder):
+        card_data = {"number": self.numbers, "expiration": expiration, "cvc": cvc, "holder": holder}
+        if card_data in df_card:
+            return True
+        else:
+            return False
+
+
 print(df)
 
 hotel_ID = input("Enter the id of hotel: ")
 hotel = Hotel(hotel_ID)
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(name, hotel)
-    print(reservation_ticket.generation())
+    credit_card = CreditCard(numbers="1234")
+    if credit_card.validate(expiration="12/26", cvc="123", holder="JOHN SMITH"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(name, hotel)
+        print(reservation_ticket.generation())
+    else:
+        print("There was a problem with your payment.")
 else:
     print("Hotel is not free")
